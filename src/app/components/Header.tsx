c65c1e14699347943962a858bb4542c23b29c592
wrdesigns-dev <wrdesigns@gmail.com>
2026-02-23 14:50:31 -0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Menu, X, ChevronDown } from 'lucide-react';
-import vitalPathLogo from './assets/vitalpath_logo_concept.png';
+import vitalPathLogo from '../../assets/vitalpath_logo_concept.png';
 
 interface HeaderProps {
   onGetStarted?: () => void;
