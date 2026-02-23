e76a0655cf2a393492b2e642b7f80b6d9c71a16c
wrdesigns-dev <wrdesigns@gmail.com>
2026-02-23 14:41:34 -0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Heart } from 'lucide-react';
-import vitalPathLogo from '@/assets/VitalPath_Logo_Concept.png';
+import vitalPathLogo from '@/assets/vitalpath_logo_concept.png';
 
 export function Footer() {
   return (
